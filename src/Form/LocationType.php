53beed91d8b26c341e7e4bd13f90aebc9ef803f0
Wodnik3z <krawczak01@gmail.com>
2024-10-19 16:03:04 +0200

    location page 5/10

--- src/Form/LocationType.php
+++ src/Form/LocationType.php
@@ -12,10 +12,22 @@ class LocationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('city')
-            ->add('country')
-            ->add('latitude')
-            ->add('longitude')
+            ->add('city', null, [
+                'attr' => [
+                    'placeholder' => 'Enter city name',
+                ],
+            ])
+            ->add('country', ChoiceType::class, [
+                'choices' => [
+                    'Poland' => 'PL',
+                    'Germany' => 'DE',
+                    'France' => 'FR',
+                    'Spain' => 'ES',
+                    'Italy' => 'IT',
+                    'United Kingdom' => 'GB',
+                    'United States' => 'US',
+                ],
+            ])   
         ;
     }
 
